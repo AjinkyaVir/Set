7fd306b68091a2ddbb0b714719942e5b558f9b05
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2020-06-09 09:08:04 +0530

    Ajinkya - Set example  TreeSet example

---
+++ src/com/company/TreeSet/Test.java
@@ -0,0 +1,34 @@
+package com.company.TreeSet;
+
+import java.util.TreeSet;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        /*
+        * Data must be homogeneous
+        * Must implements Comparable
+        *
+        * TreeSet does not allow Heterogeneous data
+        * Null value does not allowed
+        * */
+        TreeSet<String> treeSet = new TreeSet<>();
+        treeSet.add("Aju");
+        treeSet.add("Rahul");
+        treeSet.add("Sau");
+        treeSet.add("Pappu");
+        System.out.println(treeSet);
+
+
+        TreeSet<Integer> treeSet1 = new TreeSet<>();
+        treeSet1.add(20);
+        treeSet1.add(5);
+        treeSet1.add(65);
+        treeSet1.add(2);
+        System.out.println(treeSet1);
+
+
+    }
+
+}
